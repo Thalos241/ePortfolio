8ea38c1617835c0e0599d25fc36ef4dcb9b64b85
Matthew Longman <contact@matthewlongman.com>
2017-03-15 19:13:21 +0000

    init commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Matthew Longman ePortfolio</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+     <header>
+       <h1>Matthew Longman</h1>   
+     </header>
+     
+     <div id="main">
+       <article>
+         <p>This is my ePortfolio and y'all better recognize my fantastic web development capabilities!</p>
+         <p>March 13: Today I added something new</p>
+       </article>
+     </div>
+
+     <?php
+       echo('yoyoyo Sarah!!!!');
+     ?>
+     <footer>
+     
+     </footer>
+  </body>
+</html>
